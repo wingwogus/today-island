7de841576fcd7ea48c92d81d7fb20f0d6ecb32ad
wingwogus <62836746+wingwogus@users.noreply.github.com>
2025-01-18 10:49:05 +0900

    Update index.ts

--- src/libs/utils/notion/index.ts
+++ src/libs/utils/notion/index.ts
@@ -1,2 +1,3 @@
 export { getAllSelectItemsFromPosts } from "./getAllSelectItemsFromPosts"
+export { getAllSelectItemsCountFromPosts } from "./getAllSelectItemsFromPosts"
 export { filterPosts } from "./filterPosts"
