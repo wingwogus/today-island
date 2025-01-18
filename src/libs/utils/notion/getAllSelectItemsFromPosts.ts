83f114904d8ebbfc4955bcacf86e56430f773f1a
wingwogus <62836746+wingwogus@users.noreply.github.com>
2025-01-18 10:48:23 +0900

    Update getAllSelectItemsFromPosts.ts

--- src/libs/utils/notion/getAllSelectItemsFromPosts.ts
+++ src/libs/utils/notion/getAllSelectItemsFromPosts.ts
@@ -17,3 +17,25 @@ export function getAllSelectItemsFromPosts(
   })
   return itemObj
 }
+
+export function getAllSelectItemsCountFromPosts(
+    key: "tags" | "category",
+    posts: TPosts
+) {
+  const selectedPosts = posts.filter((post) => post?.[key]);
+  const items = [...selectedPosts.map((p) => p[key]).flat()];
+  const itemObj: { [itemName: string]: string[] } = {};
+  items.forEach((item, index) => {
+    if (!item) return;
+    if (itemObj[item]) {
+      itemObj[item].push(`post-${index + 1}`); // 게시글 ID는 예시로 index로 표기
+    } else {
+      itemObj[item] = [`post-${index + 1}`];
+    }
+  });
+  const itemCount: { [itemName: string]: number } = {};
+  Object.keys(itemObj).forEach((item) => {
+    itemCount[item] = itemObj[item].length;
+  });
+  return itemCount;
+}
