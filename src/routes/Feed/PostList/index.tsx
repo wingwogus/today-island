c59601056d6262fb7bae5a2e117db6cb8c1dfece
wingwogus <62836746+wingwogus@users.noreply.github.com>
2025-01-18 11:00:57 +0900

    Update index.tsx

--- src/routes/Feed/PostList/index.tsx
+++ src/routes/Feed/PostList/index.tsx
@@ -1,8 +1,9 @@
-import { useRouter } from "next/router"
-import React, { useEffect, useState } from "react"
 import PostCard from "src/routes/Feed/PostList/PostCard"
-import { DEFAULT_CATEGORY } from "src/constants"
 import usePostsQuery from "src/hooks/usePostsQuery"
+import { useRouter } from "next/router"
+import { filterPosts } from "./filterPosts"
+import { DEFAULT_CATEGORY } from "src/constants"
+import {useMemo} from "react";
 
 type Props = {
   q: string
@@ -11,44 +12,20 @@ type Props = {
 const PostList: React.FC<Props> = ({ q }) => {
   const router = useRouter()
   const data = usePostsQuery()
-  const [filteredPosts, setFilteredPosts] = useState(data)
 
   const currentTag = `${router.query.tag || ``}` || undefined
   const currentCategory = `${router.query.category || ``}` || DEFAULT_CATEGORY
   const currentOrder = `${router.query.order || ``}` || "desc"
 
-  useEffect(() => {
-    setFilteredPosts(() => {
-      let newFilteredPosts = data
-      // keyword
-      newFilteredPosts = newFilteredPosts.filter((post) => {
-        const tagContent = post.tags ? post.tags.join(" ") : ""
-        const searchContent = post.title + post.summary + tagContent
-        return searchContent.toLowerCase().includes(q.toLowerCase())
-      })
-
-      // tag
-      if (currentTag) {
-        newFilteredPosts = newFilteredPosts.filter(
-          (post) => post && post.tags && post.tags.includes(currentTag)
-        )
-      }
-
-      // category
-      if (currentCategory !== DEFAULT_CATEGORY) {
-        newFilteredPosts = newFilteredPosts.filter(
-          (post) =>
-            post && post.category && post.category.includes(currentCategory)
-        )
-      }
-      // order
-      if (currentOrder !== "desc") {
-        newFilteredPosts = newFilteredPosts.reverse()
-      }
-
-      return newFilteredPosts
+  const filteredPosts = useMemo(() => {
+    return filterPosts({
+      posts: data,
+      q,
+      tag: currentTag,
+      category: currentCategory,
+      order: currentOrder,
     })
-  }, [q, currentTag, currentCategory, currentOrder, setFilteredPosts])
+  }, [data, q, currentTag, currentCategory, currentOrder])
 
   return (
     <>
@@ -57,7 +34,7 @@ const PostList: React.FC<Props> = ({ q }) => {
           <p className="text-gray-500 dark:text-gray-300">Nothing! 😺</p>
         )}
         {filteredPosts.map((post) => (
-          <PostCard key={post.id} data={post} />
+          <PostCard key={post.id} data={post} showMedia={true} />
         ))}
       </div>
     </>
