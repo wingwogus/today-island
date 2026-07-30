edb47160654fbf88a15ffee83b81f809c4b61f08
이재현 <wingwogus@naver.com>
2026-07-31 00:26:30 +0900

    feat(profile): 블로그 프로필을 코딩 카피바라로 교체

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "이재현",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/profile/coding-capybara.jpg",
     role: "BackEnd Developer",
     bio: "I develop to change the world, even if it's just 1 percent.",
     email: "wingwogus@naver.com",
